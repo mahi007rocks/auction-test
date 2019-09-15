4a719d071b022ecf913cba1be8d1ee802d7ed2ae
Mahesh <gmahesh007babu@gmail.com>
2019-09-16 00:44:32 +0545

    sorting added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,16 @@
 class WelcomeController < ApplicationController
   def index
-  	@products = Product.page(params[:page] || 1).per(3)
+  	if params[:recent_projects].present?
+  		@products = Product.order(:name).page(params[:page] || 1).per(3)
+  	elsif params[:category_asc].present?
+  		@products = Product.joins(:category).order("category.name ASC")
+  	elsif params[:username_asc].present?
+  		@products = Product.joins(:users).order("user.username ASC")
+  	elsif params[:project_tite].present?
+  		@products = Product.order(:title)
+  	else 
+  		@products = Product.page(params[:page] || 1).per(3)
+  	end
   	respond_to do |format|
 	    format.js
 	    format.html
